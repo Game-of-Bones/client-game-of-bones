296d21f15acdcbfe4088b96f2965d78039e3e13d
EstherTapias <esthertpc93@gmail.com>
2025-10-15 10:54:31 +0200

    fix merge dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-<<<<<<< HEAD
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()],
-  
-})
->>>>>>> 1ca55dd5fb3d051a72f2b85d30f4c5b6c5e19455
