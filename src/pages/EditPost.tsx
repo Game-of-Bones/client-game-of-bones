181620ff52d1f1ffe629c27ced00d1e27fc854c6
EstherTapias <esthertpc93@gmail.com>
2025-10-16 09:32:24 +0200

    fix error edit page

--- src/pages/EditPost.tsx
+++ src/pages/EditPost.tsx
@@ -1,4 +1,3 @@
-// src/pages/EditPost.tsx
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import EditPostForm from '../components/forms/EditPostForm';
